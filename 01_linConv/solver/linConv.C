1d49d101da8aa57806f001a7d1329273ea42d08f
Aaron Howell <aaron.winter.howell@gmail.com>
2014-03-19 03:34:07 -0400

    Initial commit for OpenFoam version of the 12 steps to Navier-Stokes.

---
+++ 01_linConv/solver/linConv.C
@@ -0,0 +1,85 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 1991-2010 OpenCFD Ltd.
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+Application
+    linearConvection
+
+Description
+    Basic linear convection solver for learning by doing. Building up
+    to the Navier-Stokes equations in 12-steps. 
+
+    See: http://lorenabarba.com/blog/cfd-python-12-steps-to-navier-stokes/
+
+Author
+    Aaron Howell
+
+\*---------------------------------------------------------------------------*/
+
+#include "fvCFD.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+int main(int argc, char *argv[])
+{
+    #include "setRootCase.H"
+
+    #include "createTime.H"
+    #include "createMesh.H"
+    #include "createFields.H"
+
+    // * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+    Info<< "\nStarting time loop\n" << endl;
+
+    while (runTime.loop())
+    {
+        Info<< "Time = " << runTime.timeName() << nl << endl;
+
+        solve
+        (
+            fvm::ddt(U) 
+            + c*fvc::grad(U)->component(0)
+        );
+
+        //        U == (fvc::ddt(U) + c*fvc::grad(U)->component(0))*runTime.deltaT();
+        //        U == (c*fvc::grad(U)->component(0));
+
+
+        // volVectorField dUdx = fvc::grad(U);
+        // solve(UEqn + c*dUdx.component(0));
+        // solve(fvm::ddt(U) + c*fvc::div(U))
+
+        runTime.write();
+
+        Info<< "ExecutionTime = " << runTime.elapsedCpuTime() << " s"
+            << "  ClockTime = " << runTime.elapsedClockTime() << " s"
+            << nl << endl;
+    }
+
+    Info<< "End\n" << endl;
+
+    return 0;
+}
+
+
+// ************************************************************************* //
